66f3d28a0dc7b37e9811352c59b63437b69d3b0f
Paul R Alexander <palexander@stanford.edu>
2012-08-27 17:39:07 -0700

    Add documentation to API home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,72 @@
 class HomeController < ApplicationController
 
   def index
+    demo_ontology = Ontology.find("NCIT")
+    demo_class = Klass.find("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#Anatomic_Structure_System_or_Substance", "NCIT")
+
+    doc_intro = <<-EOS
+      The following links can be used to see the functionality available in the API.
+      Generally, you can add 'include={property}' when browsing a resource or set of resources to return a custom set of properties.
+      The API is meant to be navigated like a normal web page, by starting at the root and following the resource URLs in the 'links' list.
+    EOS
+    doc_intro.gsub!("\n", "").gsub!(/ {2,}/, " ").strip!
+
     render :json => {
       :links => { :ontologies => $BASE_UI_URL + Ontology.path.gsub("/:ontology", "") },
-      :future_links => {
-        :search => "#{$BASE_UI_URL}/search",
-        :annotator => "#{$BASE_UI_URL}/annotator",
-        :resource_index => "#{$BASE_UI_URL}/resource_index",
-        :mappings => "#{$BASE_UI_URL}/mappings",
-        :reviews => "#{$BASE_UI_URL}/reviews",
-        :notes => "#{$BASE_UI_URL}/notes",
-        :views => "#{$BASE_UI_URL}/views",
-        :categories => "#{$BASE_UI_URL}/categories",
-        :groups => "#{$BASE_UI_URL}/groups",
-        :metrics => "#{$BASE_UI_URL}/metrics",
-        :projects => "#{$BASE_UI_URL}/projects"
-      }
+      :demo => [
+        doc_intro,
+        :ontologies => {
+          :ontology => demo_ontology.canonical_path,
+          :ontology_all_properties => demo_ontology.canonical_path + "?include=all",
+          :ontology_only_name_and_acronym => demo_ontology.canonical_path + "?include=name,acronym"
+        },
+        :classes => {
+          :root_classes => demo_ontology.canonical_path + "/classes/roots",
+          :class => demo_class.canonical_path,
+          :class_all_properties => demo_class.canonical_path + "?include=all",
+          :class_only_label_and_id => demo_class.canonical_path + "?include=label,id"
+        }
+      ],
+      :planned_resources => [
+        "The following resources are planned but not yet implemented",
+        {
+          :ontology_artifacts => [
+            "/metrics",
+            "/reviews",
+            "/categories",
+            "/groups",
+            "/mappings/{mappingid}",
+            "/notes/{noteid}",
+            "/projects"
+          ],
+          :ontology_related => [
+            "/ontologies/{acronym}/reviews",
+            "/ontologies/{acronym}/projects",
+            "/ontologies/{acronym}/properties",
+            "/ontologies/{acronym}/notes",
+            "/ontologies/{acronym}/mappings",
+            "/ontologies/{acronym}/views"
+          ],
+          :class_related => [
+            "/ontologies/{acronym}/classes/{classid}",
+            "/ontologies/{acronym}/classes/{classid}/instances",
+            "/ontologies/{acronym}/classes/{classid}/instances/{instance_id}",
+            "/ontologies/{acronym}/classes/tree -- this replaces 'path to root' and provides an expanded tree structure for a given class",
+            "/ontologies/{acronym}/classes/{classid}/notes",
+            "/ontologies/{acronym}/classes/{classid}/mappings"
+          ],
+          :process_endpoints => [
+            "/search",
+            "/view_extractor",
+            "/recommender",
+            "/annotator"
+          ],
+          :general => [
+            "/resource_index",
+            "/views"
+          ]
+        }
+      ]
     }
   end
 
